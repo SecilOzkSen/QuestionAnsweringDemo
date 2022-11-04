9eb6915cf9a6a2ce3a06b147e6b4d075af9f5a45
SecilOzkSen <secilozkanoglu@gmail.com>
2022-11-04 17:16:50 +0300

    project update

--- demov2.py
+++ demov2.py
@@ -3,21 +3,27 @@ import streamlit as st
 import json
 import pandas as pd
 import tokenizers
-from sentence_transformers import SentenceTransformer,CrossEncoder, util
+from sentence_transformers import SentenceTransformer, CrossEncoder, util
 from transformers import pipeline
 from st_aggrid import GridOptionsBuilder, AgGrid
 import pickle
-
+import torch
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+import spacy
+import regex
+from typing import List
 
 st.set_page_config(layout="wide")
 
 DATAFRAME_FILE_ORIGINAL = 'policyQA_original.csv'
 DATAFRAME_FILE_BSBS = 'policyQA_bsbs_sentence.csv'
 
+
 @st.experimental_singleton(suppress_st_warning=True, show_spinner=False)
 def cross_encoder_init():
     cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
     return cross_encoder
+
 
 @st.experimental_singleton(suppress_st_warning=True, show_spinner=False)
 def bi_encoder_init():
@@ -25,15 +31,39 @@ def bi_encoder_init():
     bi_encoder.max_seq_length = 500  # Truncate long passages to 256 tokens
     return bi_encoder
 
+
 @st.experimental_singleton(suppress_st_warning=True, show_spinner=False)
 def nlp_init(auth_token, private_model_name):
-    return pipeline('question-answering', model=private_model_name, tokenizer=private_model_name, use_auth_token=auth_token,
-                   revision="main")
+    return pipeline('question-answering', model=private_model_name, tokenizer=private_model_name,
+                    use_auth_token=auth_token,
+                    revision="main")
+
 
 @st.experimental_singleton(suppress_st_warning=True, show_spinner=False)
 def nlp_pipeline_hf():
     model_name = "deepset/roberta-base-squad2"
     return pipeline('question-answering', model=model_name, tokenizer=model_name)
+
+
+@st.experimental_singleton(suppress_st_warning=True, show_spinner=False)
+def nlp_pipeline_sentence_based(auth_token, private_model_name):
+    tokenizer = RobertaTokenizer.from_pretrained(private_model_name, use_auth_token=auth_token)
+    model = RobertaForSequenceClassification.from_pretrained(private_model_name, use_auth_token=auth_token)
+    return tokenizer, model
+
+
+@st.cache(hash_funcs={tokenizers.Tokenizer: lambda _: None, tokenizers.AddedToken: lambda _: None,
+                      regex.Pattern: lambda _: None}, show_spinner=False)
+def load_models_sentence_based(auth_token, private_model_name, private_model_name_base):
+    bi_encoder = bi_encoder_init()
+    cross_encoder = cross_encoder_init()
+    # OLD MODEL
+    # nlp = nlp_init(auth_token, private_model_name)
+    # nlp_hf = nlp_pipeline_hf()
+    policy_qa_tokenizer, policy_qa_model = nlp_pipeline_sentence_based(auth_token, private_model_name)
+    asnq_tokenizer, asnq_model = nlp_pipeline_sentence_based(auth_token, private_model_name_base)
+
+    return bi_encoder, cross_encoder, policy_qa_tokenizer, policy_qa_model, asnq_tokenizer, asnq_model
 
 
 @st.cache(hash_funcs={tokenizers.Tokenizer: lambda _: None, tokenizers.AddedToken: lambda _: None}, show_spinner=False)
@@ -44,6 +74,7 @@ def load_models(auth_token, private_model_name):
     nlp_hf = nlp_pipeline_hf()
 
     return bi_encoder, cross_encoder, nlp, nlp_hf
+
 
 def context():
     bi_encoder = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1', device='cpu')
@@ -73,14 +104,15 @@ def load_dataframes():
     data_bsbs = data_bsbs.sample(frac=1).reset_index(drop=True)
     return data_original, data_bsbs
 
+
 def search(question, corpus_embeddings, contexes, bi_encoder, cross_encoder):
-    #Semantic Search (Retrieve)
+    # Semantic Search (Retrieve)
     question_embedding = bi_encoder.encode(question, convert_to_tensor=True)
     hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=100)
     if len(hits) == 0:
         return []
     hits = hits[0]
-    #Rerank - score all retrieved passages with cross-encoder
+    # Rerank - score all retrieved passages with cross-encoder
     cross_inp = [[question, contexes[hit['corpus_id']]] for hit in hits]
     cross_scores = cross_encoder.predict(cross_inp)
 
@@ -91,21 +123,48 @@ def search(question, corpus_embeddings, contexes, bi_encoder, cross_encoder):
     # Output of top-5 hits from re-ranker
     hits = sorted(hits, key=lambda x: x['cross-score'], reverse=True)
     top_5_contexes = []
+    top_5_scores = []
     for hit in hits[0:5]:
         top_5_contexes.append(contexes[hit['corpus_id']])
-    return top_5_contexes
+        top_5_scores.append(hit['cross-score'])
+    return top_5_contexes, top_5_scores
+
 
 def paragraph_embeddings():
     paragraphs = load_paragraphs()
     context_embeddings = bi_encoder.encode(paragraphs, convert_to_tensor=True, show_progress_bar=True)
     return context_embeddings, paragraphs
 
+
 def retrieve_rerank_pipeline(question, context_embeddings, paragraphs, bi_encoder, cross_encoder):
-    top_5_contexes = search(question, context_embeddings, paragraphs, bi_encoder, cross_encoder)
-    return top_5_contexes
+    top_5_contexes, top_5_scores = search(question, context_embeddings, paragraphs, bi_encoder, cross_encoder)
+    return top_5_contexes, top_5_scores
+
 
 def qa_pipeline(question, context, nlp):
     return nlp({'question': question.strip(), 'context': context})
+
+
+def qa_pipeline_sentence(question, context, model, tokenizer):
+    sentences_doc = spacy_nlp(context)
+    candidate_sentences = []
+    for sentence in sentences_doc.sents:
+        tokenized = tokenizer(f"{question} [SEP] {sentence.text}", padding=True, truncation=True, return_tensors='pt')
+        output = model(**tokenized)
+        res = torch.argmax(output[0], dim=-1)
+        res_list = res.cpu().detach().numpy()
+        if res_list[0] == 1:
+            candidate_sentences.append(sentence)
+    return candidate_sentences
+
+
+def candidate_sentence_controller(sentences):
+    if sentences is None or len(sentences) == 0:
+        return ""
+    if len(sentences) == 1:
+        return sentences[0]
+    return sentences
+
 
 def interactive_table(dataframe):
     gb = GridOptionsBuilder.from_dataframe(dataframe)
@@ -131,7 +190,7 @@ def interactive_table(dataframe):
 
 def qa_main_widgetsv2():
     st.title("Question Answering Demo")
-    col1, col2, col3 = st.columns([2,1,1])
+    col1, col2, col3 = st.columns([2, 1, 1])
     with col1:
         form = st.form(key='first_form')
         question = form.text_area("What is your question?:", height=200)
@@ -142,22 +201,42 @@ def qa_main_widgetsv2():
             st.session_state.form_submit = True
         if st.session_state.form_submit and question != '':
             with st.spinner(text='Related context search in progress..'):
-                top_5_contexes = retrieve_rerank_pipeline(question.strip(), context_embeddings, paragraphs, bi_encoder,
-                                                          cross_encoder)
+                top_5_contexes, top_5_scores = retrieve_rerank_pipeline(question.strip(), context_embeddings,
+                                                                        paragraphs, bi_encoder,
+                                                                        cross_encoder)
             if len(top_5_contexes) == 0:
                 st.error("Related context not found!")
                 st.session_state.form_submit = False
             else:
                 with st.spinner(text='Now answering your question..'):
                     for i, context in enumerate(top_5_contexes):
-                        answer_trained = qa_pipeline(question, context, nlp)
-                        answer_base = qa_pipeline(question, context, nlp_hf)
-                        st.markdown(f"## Related Context - {i + 1}:")
+                        #  answer_trained = qa_pipeline(question, context, nlp)
+                        # answer_base = qa_pipeline(question, context, nlp_hf)
+                        answer_trained = qa_pipeline_sentence(question, context, policy_qa_model, policy_qa_tokenizer)
+                        answer_base = qa_pipeline_sentence(question, context, asnq_model, asnq_tokenizer)
+                        st.markdown(f"## Related Context - {i + 1} (score: {top_5_scores[i]:.2f})")
                         st.markdown(context)
                         st.markdown("## Answer (trained):")
-                        st.markdown(answer_trained['answer'])
-                        st.markdown("## Answer (deepset/roberta-base-squad2):")
-                        st.markdown(answer_base['answer'])
+                        print(answer_trained)
+                        if answer_trained is None:
+                            st.markdown("")
+                        elif isinstance(answer_trained, List):
+                            for i,answer in enumerate(answer_trained):
+                                st.markdown(f"### Answer Option {i+1}:")
+                                st.markdown(answer)
+                        else:
+                            st.markdown(answer_trained)
+                        # st.markdown(answer_trained['answer'])
+                        st.markdown("## Answer (roberta-base-asnq):")
+                        print(answer_base)
+                        if answer_base is None:
+                            st.markdown("")
+                        elif isinstance(answer_base, List):
+                            for i,answer in enumerate(answer_base):
+                                st.markdown(f"### Answer Option {i+1}:")
+                                st.markdown(answer)
+                        else:
+                            st.markdown(answer_base)
                         st.markdown("""---""")
 
     with col2:
@@ -201,13 +280,18 @@ def qa_main_widgetsv2():
 def load():
     context_embeddings, paragraphs = load_paragraphs()
     dataframe_original, dataframe_bsbs = load_dataframes()
-    bi_encoder, cross_encoder, nlp, nlp_hf = copy.deepcopy(load_models(st.secrets["AUTH_TOKEN"], st.secrets["MODEL_NAME"]))
-    return context_embeddings, paragraphs, dataframe_original, dataframe_bsbs, bi_encoder, cross_encoder, nlp, nlp_hf
+    spacy_nlp = spacy.load('en_core_web_sm')
+    # bi_encoder, cross_encoder, nlp, nlp_hf = copy.deepcopy(load(st.secrets["AUTH_TOKEN"], st.secrets["MODEL_NAME"]))
+    bi_encoder, cross_encoder, policy_qa_tokenizer, policy_qa_model, asnq_tokenizer, asnq_model \
+        = copy.deepcopy(
+        load_models_sentence_based(st.secrets["AUTH_TOKEN"], st.secrets["MODEL_NAME"], st.secrets["MODEL_NAME_BASE"]))
+    return context_embeddings, paragraphs, dataframe_original, dataframe_bsbs, bi_encoder, cross_encoder, policy_qa_tokenizer, policy_qa_model, asnq_tokenizer, asnq_model, spacy_nlp
 
 
- #   save_dataframe()
-context_embeddings, paragraphs, dataframe_original, dataframe_bsbs, bi_encoder, cross_encoder, nlp, nlp_hf = load()
+#   save_dataframe()
+# context_embeddings, paragraphs, dataframe_original, dataframe_bsbs, bi_encoder, cross_encoder, nlp, nlp_hf = load()
+context_embeddings, paragraphs, dataframe_original, dataframe_bsbs, bi_encoder, cross_encoder, policy_qa_tokenizer, policy_qa_model, asnq_tokenizer, asnq_model, spacy_nlp = load()
 qa_main_widgetsv2()
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    context()
